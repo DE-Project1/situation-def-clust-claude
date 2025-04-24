4e12e399c12392cf8037cf8422fcc90b46cfb5ba
Kaeun Han <124423333+kkaeunhan@users.noreply.github.com>
2025-04-25 01:00:48 +0900

    refactor : inertia, silhouette_score 추가, 함수 내 변수명 수정
    - Inertia(관성), 실루엣 점수를 통한 클러스터링 품질 점검 코드 추가
    
    - shadows name warning 해결

--- step2_clustering/step2_cluster_clustering.py
+++ step2_clustering/step2_cluster_clustering.py
@@ -3,6 +3,7 @@
 import json
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 from bson import ObjectId
 from mongo.mongo_connector import MongoConnector
 from dotenv import load_dotenv
@@ -13,15 +14,15 @@ load_dotenv()
 def load_vectors_from_merged_json(path: str):
     with open(path, 'r', encoding='utf-8') as f:
         merged = json.load(f)
-    ids = list(merged.keys())
+    vector_ids = list(merged.keys())
     vectors = np.array(list(merged.values()))
-    return ids, vectors
+    return vector_ids, vectors
 
 # 2. 클러스터링 결과를 MongoDB에 저장
-def upload_cluster_results_to_mongo(id_to_cluster: dict):
+def upload_cluster_results_to_mongo(cluster_map : dict):
     mongo = MongoConnector()
     updates = []
-    for _id, cluster in id_to_cluster.items():
+    for _id, cluster in cluster_map.items():
         try:
             updates.append({
                 "_id": ObjectId(_id),
@@ -39,15 +40,23 @@ def upload_cluster_results_to_mongo(id_to_cluster: dict):
 
 # 3. 전체 실행 흐름
 if __name__ == "__main__":
+    # (1) 로딩
     print("🚀 merged_vectors.json 로딩 중...")
-    ids, embeddings = load_vectors_from_merged_json("merged_vectors.json")
+    ids, embeddings = load_vectors_from_merged_json("./step2_clustering/merged_vectors.json")
     print(f"✅ 벡터 수: {len(ids)}")
 
+    # (2) KMeans 클러스터링
     print("🔍 KMeans 클러스터링 중...")
-    kmeans = KMeans(n_clusters=40, random_state=42, n_init="auto")
+    kmeans = KMeans(n_clusters=20, random_state=42, n_init="auto")
     labels = kmeans.fit_predict(embeddings)
 
     id_to_cluster = {id_: int(cluster) for id_, cluster in zip(ids, labels)}
 
+    # (3) 품질 평가
+    print(f"📉 관성(Inertia): {kmeans.inertia_:.2f}")
+    sil_score = silhouette_score(embeddings, labels)
+    print(f"📊 실루엣 점수: {sil_score:.4f}")
+
+    # (4) MongoDB 업로드
     print("⬆️ MongoDB에 클러스터 결과 업로드 중...")
     upload_cluster_results_to_mongo(id_to_cluster)
