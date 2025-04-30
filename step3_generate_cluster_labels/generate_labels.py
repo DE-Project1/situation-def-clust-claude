b06cbd5e6942ca53a8c4eb194dee65063fcadd9b
Kaeun Han <124423333+kkaeunhan@users.noreply.github.com>
2025-05-01 02:27:22 +0900

    remove : step3_label에 한 번에 연결

--- step3_generate_cluster_labels/generate_labels.py
+++
@@ -1,6 +0,0 @@
-from claude_api_wrapper import ClaudeAPIWrapper
-
-async def generate_cluster_label(definition_list):
-    claude = ClaudeAPIWrapper()
-    label = await claude.generate_cluster_label_async(definition_list)
-    return label
